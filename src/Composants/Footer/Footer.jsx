18edf6786663549d1b6691ae7de3ca2195c49b41
SEKONGO <sekongomoussa77@gmail.com>
2025-10-08 09:27:29 +0000

    ok

--- src/Composants/Footer/Footer.jsx
+++ src/Composants/Footer/Footer.jsx
@@ -6,8 +6,8 @@ let Footer = ()=>{
 
     return <>
              <div className='h-[100px] bg-white dark:bg-black'></div>
-            <div className='h-[80px] flex w-full justify-center items-center bg-sky-700 dark:bg-neutral-900 text-black dark:text-white'>
-                <p>Copyright © 2025 par Sekongo Moussa | Tout droits reservés.</p>
+            <div className='h-[80px] flex text-[11px] sm:text-[20px] w-full justify-center items-center bg-sky-700 dark:bg-neutral-900 text-black dark:text-white'>
+                <p className='justify-center items-center  w-[95%] sm:w-full flex'>Copyright © 2025 par Sekongo Moussa | Tout droits reservés.</p>
             </div>
             </>
 }
